88a03f2527dfd82216a29e8a723bf31907fd1cf6
Darlan Tane <112576210+darlantane@users.noreply.github.com>
2026-03-06 12:16:25 +0100

    website test

---
+++ pokepedia.py
@@ -0,0 +1,80 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+import time
+
+class TestPokepedia:
+
+    def setup(self):
+        """Initialisation du navigateur"""
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.wait = WebDriverWait(self.driver, 15)
+
+    def test_homepage_load(self):
+        """Test 1 : Vérifier que la page d'accueil charge correctement"""
+        self.driver.get("https://www.pokepedia.fr/")
+
+        try:
+            self.wait.until(
+                EC.presence_of_element_located((By.TAG_NAME, "body"))
+            )
+            print("Page d'accueil chargée avec succès")
+        except TimeoutException:
+            print("Erreur : la page d'accueil ne s'est pas chargée")
+
+    def test_pokemon_search(self):
+        """Test 2 : Effectuer une recherche Pokémon"""
+        self.driver.get("https://www.pokepedia.fr/")
+
+        try:
+            # Accepter les cookies si présent
+            try:
+                cookie_button = self.wait.until(
+                    EC.element_to_be_clickable((By.ID, "onetrust-accept-btn-handler"))
+                )
+                cookie_button.click()
+                print("Cookies acceptés")
+            except:
+                pass
+
+            # Localiser le champ de recherche
+            search_input = self.wait.until(
+                EC.presence_of_element_located((By.ID, "searchInput"))
+            )
+
+            search_input.clear()
+            search_input.send_keys("Pikachu")
+            search_input.send_keys(Keys.RETURN)
+
+            print("Recherche lancée")
+
+            # Attendre l'apparition du titre de la page Pokémon
+            self.wait.until(
+                EC.presence_of_element_located((By.ID, "firstHeading"))
+            )
+
+            print("Page Pokémon affichée")
+
+        except TimeoutException:
+            print("Erreur : la page Pokémon ne s'est pas chargée")
+
+    def teardown(self):
+        """Fermeture du navigateur"""
+        time.sleep(3)
+        self.driver.quit()
+        print("Navigateur fermé")
+
+
+if __name__ == "__main__":
+    test = TestPokepedia()
+    test.setup()
+
+    try:
+        test.test_homepage_load()
+        test.test_pokemon_search()
+    finally:
+        test.teardown()
